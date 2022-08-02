de97ce12dca3ba3018aead9efd8360e44a2fe9ff
Sourcery AI <>
2022-08-02 19:15:23 +0000

    'Refactored by Sourcery'

--- demo_dao.py
+++ demo_dao.py
@@ -19,8 +19,11 @@ class DemoDao():
             user_name = self.__body.get('username')
             message = self.__body.get('message')
             current_now = datetime.datetime.now()
-            my_message = "Hi " + user_name + ".My name is Ramit. " + " Here is your message: " + message + ". The time of your message ==>" + current_now.strftime("%Y-%m-%d %H:%M")
-            return my_message
+            return (
+                f"Hi {user_name}.My name is Ramit.  Here is your message: {message}. The time of your message ==>"
+                + current_now.strftime("%Y-%m-%d %H:%M")
+            )
+
         except Exception as exception:
             LoggerUtility.log_error(exception)
             raise Exception(exception)
@@ -28,8 +31,13 @@ class DemoDao():
     def favorite_shows(self):
         """favorite_shows for ramit."""
         try:
-            favorite_shows = "Here is the list of My Favorite Shows: " + "BBT, " + "Young Sheldon," + " Suits"
-            return favorite_shows
+            return (
+                "Here is the list of My Favorite Shows: "
+                + "BBT, "
+                + "Young Sheldon,"
+                + " Suits"
+            )
+
         except Exception as exception:
             LoggerUtility.log_error(exception)
             raise Exception(exception)
